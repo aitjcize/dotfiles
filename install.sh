6218dd799e26daf711953c524e5c33993e5ac257
Wei-Ning Huang <aitjcize@gmail.com>
2020-09-14 13:45:18 +0800

    update

--- install.sh
+++ install.sh
@@ -32,6 +32,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_DIR/zsh-
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_DIR/zsh-autosuggestions.git
 
 # vim
-mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +BundleInstall
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall
